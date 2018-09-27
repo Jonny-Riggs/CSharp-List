2063f534e3ec44cb551e34ffaf1b205bf4883e23
Jonny-Riggs <jonny.riggs615@gmail.com>
2018-09-27 15:44:56 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> planetListTwo = new List<string>(){"Saturn", "Uranus"};
+
+            planetList.AddRange(planetListTwo);
+
+            planetList.Insert(1, "Earth");
+            planetList.Insert(2, "Venus");
+
+            planetList.Add("Pluto");
+
+            List<string> rockyPlanets = planetList.GetRange(0, 6);
+
+            planetList.Remove("Pluto");
+
+
+            // foreach (Object rocky in rockyPlanets)
+            // {
+            //     Console.WriteLine(rocky);
+            // }
+
+            foreach (Object planet in planetList)
+            {
+                Console.WriteLine(planet);
+            }
+
+
+        }
+    }
+}
